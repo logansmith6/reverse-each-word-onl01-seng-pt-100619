cf3203f4716c138eb8d3bc368ab8e527d1464994
Logan Smith <logsmith06@gmail.com>
2019-10-18 21:27:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,13 @@
 
 def reverse_each_word(string)
 
-string.split(",")
-string.each do |word|
+reverse_word = []
+reverse_word = string.split(",")
+reverse_word.each do |word|
 
 word.reverse!
 
 end
-
+string = reverse_word.join
 
 end
