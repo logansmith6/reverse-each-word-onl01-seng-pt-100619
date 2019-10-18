f780df5a2feaf4c187dfa080cc871482be821ccf
Logan Smith <logsmith06@gmail.com>
2019-10-18 21:24:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,14 @@ string.each do |word|
 word.split(",")
 x = word.size
 y = 0
+size = word.size
+size.times do
 reversed_word[x] = word[y]
 x -= 1
 y += 1
+end
 
-string[word] = reversed_word
+string[word] = reversed_word.join
 
 
 
