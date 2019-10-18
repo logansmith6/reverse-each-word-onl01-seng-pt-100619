d5084ba6b02b3505056d5461b9a22cc366b32456
Logan Smith <logsmith06@gmail.com>
2019-10-18 21:26:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,26 +1,12 @@
 
 def reverse_each_word(string)
 
-reversed_word = []
-string = string.split(",")
+string.split(",")
 string.each do |word|
-word.split(",")
-x = word.size
-y = 0
-size = word.size
-size.times do
-reversed_word[x] = word[y]
-x -= 1
-y += 1
-end
 
-string[word] = reversed_word.join
-
-
-
-
+word.reverse!
 
 end
-return string.join
+
 
 end
